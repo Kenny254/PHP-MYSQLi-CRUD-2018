60bf82c5dcba629f985a3dcc84a196a950424bef
Ken Ronoh <kennethronoh1@gmail.com>
2018-09-28 04:54:04 -0700

    Create loadMore.php
    Load more button

---
+++ loadMore.php
@@ -0,0 +1,8 @@
+<?php
+
+<div class="show_more_main" id="show_more_main<?php echo $postID; ?>">
+        <span id="<?php echo $postID; ?>" class="show_more" title="Load more posts">Show more</span>
+        <span class="loding" style="display: none;"><span class="loding_txt">Loading...</span></span>
+</div>
+//Requires a style.css for load more button and animation.
+?>
