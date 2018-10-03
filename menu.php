fbdd75d2986bda41e98ebcfd90571116e54e5a12
Ken Ronoh <kennethronoh1@gmail.com>
2018-10-03 13:07:24 -0700

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,20 +1,26 @@
 APPLIANCES
-SELECT * FROM `brands` WHERE name IN ('Samsung', 'Sony', 'Lg','Ramtons', 'Mica', 'Von Point', 'Sterling', 'Philips', 'Ampex', 'Generic', 'Hisense');");
+SELECT * FROM `brands` WHERE name IN ('Samsung', 'lG', 'Sony', 'Ramtons', 'Mica', 'Von Point', 'Sterling', 'Philips', 'Ampex', 'Generic', 'Hisense');");
     
 FASHION -MEN
-SELECT * FROM `brands` WHERE name IN ('Samsung', 'Sony', 'Lg','Ramtons', 'Mica', 'Von Point', 'Sterling', 'Philips', 'Ampex', 'Generic', 'Hisense');");
+SELECT * FROM `brands` WHERE name IN (Gucci', 'Luis Vuiton', 'Adidas','Puma', 'Playboy', 'Generic', 'Nike', 'Arman');");
  
 FASHION -WOMEN
-SELECT * FROM `brands` WHERE name IN ('Samsung', 'Sony', 'Lg','Ramtons', 'Mica', 'Von Point', 'Sterling', 'Philips', 'Ampex', 'Generic', 'Hisense');");
+SELECT * FROM `brands` WHERE name IN (Gucci', 'Luis Vuiton', 'Adidas','Puma', 'Playboy', 'Generic', 'Nike','Arman');");
  
 FASHION -KIDS
-SELECT * FROM `brands` WHERE name IN ('Samsung', 'Sony', 'Lg','Ramtons', 'Mica', 'Von Point', 'Sterling', 'Philips', 'Ampex', 'Generic', 'Hisense');");
+SELECT * FROM `brands` WHERE name IN (Gucci', 'Luis Vuiton', 'Adidas','Puma', 'Playboy', 'Generic', 'Nike','Arman');");
+
+FASHION -KIDSline
+SELECT * FROM `brands` WHERE name IN (Gucci', 'Luis Vuiton', 'Adidas','Puma', 'Playboy', 'Generic', 'Nike','Arman');");
+
+FASHION -popular brands
+SELECT * FROM `brands` WHERE name IN (Adidas', 'Luis Vuiton', 'Gucci','Puma', 'Playboy', 'Polo', 'Nike','Converse');");
  
 SMARTPHONES
-SELECT * FROM `brands` WHERE name IN ('Samsung', 'Sony', 'Lg','Ramtons', 'Mica', 'Von Point', 'Sterling', 'Philips', 'Ampex', 'Generic', 'Hisense');");
+SELECT * FROM `brands` WHERE name IN ('Samsung', 'Iphone', 'LG','Huawei', 'Sony Ericson', 'Tecno', 'Oppo', 'Wiko', 'Infinix', 'Itel', 'Generic');");
  
 COMPUTING
-SELECT * FROM `brands` WHERE name IN ('Samsung', 'Sony', 'Lg','Ramtons', 'Mica', 'Von Point', 'Sterling', 'Philips', 'Ampex', 'Generic', 'Hisense');");
+SELECT * FROM `brands` WHERE name IN ('Hp', 'Samsung', 'Lenovo','Toshiba', 'Dell', 'Acer', 'Desktops', 'Laptops', 'Apple', 'Displays', 'Scanners', 'Photocopiers', 'Accessories');");
 
 FASHION -MEN
 SELECT * FROM `brands` WHERE name IN ('Samsung', 'Sony', 'Lg','Ramtons', 'Mica', 'Von Point', 'Sterling', 'Philips', 'Ampex', 'Generic', 'Hisense');");
