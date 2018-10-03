350c7a81efeb78695ee83614374b012c2889750f
Ken Ronoh <kennethronoh1@gmail.com>
2018-10-03 11:53:23 -0700

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,14 +1,3 @@
-<?php
-                                            // Include the database configuration file
-                                            include '../server/dbConfig.php';
+
+ $query = $db->query("SELECT * FROM `brands` WHERE name IN ('Samsung', 'Sony', 'Lg','Ramtons', 'Mica', 'Von Point', 'Sterling', 'Philips', 'Ampex', 'Generic', 'Hisense');");
     
-                                            // Get records from the database
-                                            $query = $db->query("SELECT * FROM `brands` WHERE name IN ('Samsung', 'Sony', 'Lg','Ramtons', 'Mica', 'Von Point', 'Sterling', 'Philips', 'Ampex', 'Generic', 'Hisense');");
-    
-                                            if($query->num_rows > 0){ 
-                                            while($row = $query->fetch_assoc()){ 
-            
-                                            ?>
-											<li><a href="product_filter.php?id=<?php $id =$row['id']; echo $id;?>"><?php echo $row['name']; ?></a></li>
-											<?php } ?>
-					                    	<?php } ?>
